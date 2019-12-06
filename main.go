74f19037b4e7ee515e9115aa1540b971e98dced0
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-12-06 08:26:10 -0700

    idea removal step 1

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("testing version control function")
+	fmt.Println("hello")
+
+	x := "':='" // declares there is a variable x and assigns it a value.
+	fmt.Println(x + "is the short declaration operator.")
+
+
 }
